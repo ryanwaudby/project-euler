3961e244de9f8567b5acff58fbcd7f8c9b82d462
Ryan Lewis Waudby <ryan@ryanwaudby.co.uk>
2014-10-21 16:45:25 +0100

    refactor to problem 1

--- probelm_1.rb
+++ probelm_1.rb
@@ -2,9 +2,5 @@
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
 sum = 0
-
-1000.times do |number|
-  sum += number if number % 3 == 0 || number % 5 == 0
-end
-
+1000.times { |number| sum += number if number % 3 == 0 || number % 5 == 0 }
 p sum
