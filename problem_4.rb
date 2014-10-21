757d931fdd53bae211a5f729614f2754fe9429b1
Ryan Lewis Waudby <ryan@ryanwaudby.co.uk>
2014-10-21 22:49:56 +0100

    completed problem 4

---
+++ problem_4.rb
@@ -0,0 +1,17 @@
+#A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+#Find the largest palindrome made from the product of two 3-digit numbers.
+
+RANGE = (0..999)
+
+def palindrome?(number)
+  number.to_s == number.to_s.reverse
+end
+
+largest = 0
+
+RANGE.each { |left| RANGE.each { |right|
+  calculated = left * right
+  largest = calculated if palindrome?(calculated) && (calculated > largest)
+}}
+
+p largest
