0c3e16c47720a6bf5a9bbf349e17900b83d48f1e
Ryan Lewis Waudby <ryan@ryanwaudby.co.uk>
2014-12-15 20:11:19 +0000

    update problem 1

--- problem_1.rb
+++ problem_1.rb
@@ -1,8 +1,4 @@
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
-sum = 0
-1000.times { |number| sum += number if number % 3 == 0 || number % 5 == 0 }
-p sum
-
-p (1..999).inject(0) { |total, i| total + i }
+p (1..999).to_a.inject(0) { |s, i| (i % 3 == 0 || i % 5 == 0) ? s += i : s }
